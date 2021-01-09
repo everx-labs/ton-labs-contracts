94e9c9c7b080de46a08caa2d2678c32ea32d3ac6
mikhail <mikhail.ushakov@yahoo.com>
2021-01-09 03:03:39 +0300

    deployall.sh update

--- governance/SMV/SMVDeBot/deployall.sh
+++ governance/SMV/SMVDeBot/deployall.sh
@@ -2,15 +2,21 @@
 set -e
 #input
 export CLI_PATH=~/test/tonos-cli/target/release
-export LOCAL_GIVER_PATH=~/givers/local_giver/
-export NET_GIVER_PATH=~/givers/net_giver/
+export LOCAL_GIVER_PATH=~/depool_debot/local_giver/
+export NET_GIVER_PATH=~/depool_debot/net_giver/
 
-#export DEPLOY_LOCAL=1
 export SUPER_ROOT_ADDRESS=0:4e1bcac1f07ab3d139fac4c524716e66c94f74bd13b5608a8ef579a7f3b06c57
 export SMV_STAT_ADDRESS=0:6a01c3ebee13eba6732929e54bb43996ed90c9e90ebcdf4333962c95a3f4bd6b
-export MSIG_DEBOT_ADDRESS=0:d2eb2fae3bb54d131814ffcf3f176b915eaf71f113b1701d676aba3f6b4d1e4d
 
+cd msigHelper
+./deploy.sh
+cd ..
 
+export MSIG_DEBOT_ADDRESS=$(cat msigHelper/address.log)
+
+cd deployProposal
+./deploy.sh
+cd ..
 
 cd smvStats
 ./deploy.sh
