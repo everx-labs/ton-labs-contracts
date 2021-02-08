a76d5e3adf55266e947c0b04aac1e5824c97a0c4
Boris Ivanovsky <BorisI@users.noreply.github.com>
2021-02-08 19:24:45 +0300

    Simplified contest data access

--- solidity/contest/IContestData.sol
+++ solidity/contest/IContestData.sol
@@ -1,41 +1,40 @@
-pragma solidity >= 0.6.0;
+pragma ton-solidity ^0.36.0;
 
-import "IBaseData.sol";
-interface IContestData {
+enum Stage { Undefined, Setup, Contest, Vote, Finalize, Rank, Reward, Finish, Reserved, Last }
 
-    /* General contest information */
-    struct ContestInfo {
-        uint32 gid;         // Contract global ID
-        string title;       // Title of the contract
-        string link;        // Link to the document location
-        uint hash;          // Hash of the proposal
-    }
+/* General contest information */
 
-    struct Juror {
-        uint key;           // Juror's public key
-        address addr;       // Juror's address
-    }
+struct ContestInfo {
+    uint32 gid;         // Contract global ID
+    string title;       // Title of the contract
+    string link;        // Link to the document location
+    uint hashCode;      // Hash of the proposal
+}
 
-    /* Timeline of the contest */
-    struct ContestTimeline {
-        uint32 createdAt;     // Contest contract creation
-        uint32 contestStarts; // Accepts contest entries
-        uint32 contestEnds;   // End of the acceptance period
-        uint32 votingEnds;    // End of the voting period
-    }
+struct Juror {
+    uint key;       // Juror's public key
+    address addr;   // Juror's address
+}
 
-    struct ContestStage {
-        uint32 mask;
-        uint32 notifyAt;
-    }
+/* Timeline of the contest */
+struct ContestTimeline {
+    uint32 createdAt;     // Contest contract creation
+    uint32 contestStarts; // Accepts contest entries
+    uint32 contestEnds;   // End of the acceptance period
+    uint32 votingEnds;    // End of the voting period
+}
 
-    /* Individual contest entry */
-    struct ContenderInfo {
-        address addr;       // Rewards go there
-        string forumLink;   // forum post link
-        string fileLink;    // PDF document link
-        uint hash;          // hash of the PDF
-        address contact;    // Surf address contact (optional)
-        uint32 appliedAt;   // Timestamp of the entry arrival        
-    }
+struct ContestStage {
+    uint32 mask;
+    uint32 notifyAt;
+}
+
+/* Individual contest entry */
+struct ContenderInfo {
+    address addr;       // Rewards go there
+    string forumLink;   // forum post link
+    string fileLink;    // PDF document link
+    uint hashCode;      // hash of the PDF
+    address contact;    // Surf address contact (optional)
+    uint32 appliedAt;   // Timestamp of the entry arrival        
 }
