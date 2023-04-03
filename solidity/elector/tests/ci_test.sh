a68e59a21cc9d1d0956cfd4d32bb466bfe023a03
karneges <karneges2012@gmail.com>
2023-04-03 13:51:02 +0300

    start

--- solidity/elector/tests/ci_test.sh
+++ solidity/elector/tests/ci_test.sh
@@ -4,17 +4,17 @@ echo "Recompile contracts..."
 /o/projects/broxus/elector/sold --version
 rm -rf rebuild
 mkdir rebuild
-cp ../*sol rebuild/
-sed '/pragma ton-solidity/a pragma upgrade func;' rebuild/Config.sol > rebuild/Config.Update.sol
-sed '/pragma ton-solidity/a pragma upgrade func;' rebuild/Elector.sol > rebuild/Elector.Update.sol
+cp ../*tsol rebuild/
+sed '/pragma ton-solidity/a pragma upgrade func;' rebuild/Config.tsol > rebuild/Config.Update.tsol
+sed '/pragma ton-solidity/a pragma upgrade func;' rebuild/Elector.tsol > rebuild/Elector.Update.tsol
 cd rebuild
-for src in $(ls [!I]*.sol | sed -E 's/\.sol//g')
+for src in $(ls [!I]*.tsol | sed -E 's/\.tsol//g')
 do
     if [ "$src" != "Common" ]
     then
         echo
-        echo "Process file $src.sol ..."
-        /o/projects/broxus/elector/sold $src.sol
+        echo "Process file $src.tsol ..."
+        /o/projects/broxus/elector/sold $src.tsol
         # dbg=$src.debug.json
         # map=$(jq -e '.map' $dbg)
         # if "$map" != "null"
@@ -27,7 +27,7 @@ do
         #     jq '.map | . +' $dbg > temp.json
         #     mv temp.json $dbg
         # fi
-        echo "Done $src.sol"
+        echo "Done $src.tsol"
     fi
 done
 cd ..
